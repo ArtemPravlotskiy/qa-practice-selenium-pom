04b76092ea61c550007961d4c011657f93de7b66
Artsiom Praulotski <144160855+ArtemPravlotskiy@users.noreply.github.com>
2025-08-23 17:50:54 +0300

    Add error hook fixture

--- conftest.py
+++ conftest.py
@@ -1,4 +1,8 @@
+import os.path
+from datetime import datetime
+
 import pytest
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.edge.service import Service as EdgeService
@@ -77,3 +81,39 @@ def pytest_generate_tests(metafunc):
 
         if parallel and browser_option == "all":
             print(f"\n[INFO] Parallel mode ON. Use: pytest -n {len(browsers)}")
+
+ARTIFACTS_DIR = "artifacts"
+os.makedirs(ARTIFACTS_DIR, exist_ok=True)
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+
+    if report.when == "call" and report.failed:
+        driver = item.funcargs.get("driver")
+        if driver:
+            timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+            test_name = item.name
+
+            # screenshot
+            screenshot_path = os.path.join(ARTIFACTS_DIR, f"{test_name}_{timestamp}.png")
+            driver.save_screenshot(screenshot_path)
+            logging.error(f"Screenshot save: {screenshot_path}")
+
+            # HTML
+            html_path = os.path.join(ARTIFACTS_DIR, f"{test_name}_{timestamp}.html")
+            with open(html_path, "w", encoding="utf-8") as f:
+                f.write(driver.page_source)
+            logging.error(f"HTML save: {html_path}")
+
+            # Browser logs
+            try:
+                log_path = os.path.join(ARTIFACTS_DIR, f"{test_name}_{timestamp}.log")
+                logs = driver.get_log("browser")
+                with open(log_path, "w", encoding="utf-8") as f:
+                    for entry in logs:
+                        f.write(f"{entry['level']} - {entry['message']}\n")
+                logging.error(f"Browser logs save: {log_path}")
+            except Exception as e:
+                logging.warning(f"There is not browser logs: {e}")
