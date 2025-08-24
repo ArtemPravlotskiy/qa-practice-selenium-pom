38923f505ec3692c94ae32d46df89d3f685dd3d3
Artsiom Praulotski <144160855+ArtemPravlotskiy@users.noreply.github.com>
2025-08-24 18:24:39 +0300

    Add Main_page.py

---
+++ pages/Main_page.py
@@ -0,0 +1,23 @@
+import logging
+
+from conftest import driver
+from pages.Base_page import BasePage
+
+logger = logging.getLogger(__name__)
+
+class MainPage(BasePage):
+
+    PAGE_URL = "https://www.qa-practice.com/"
+
+    """Locators"""
+
+    def open(self):
+        self.driver.get(self.PAGE_URL)
+        logger.info(f"Open page: {self.PAGE_URL}")
+
+    def get_content_urls(self):
+        elements = self.driver.find_elements("css selector", "#content li a")
+        urls = [element.get_attribute("href") for element in elements]
+        logger.info(f"Found {len(urls)} content links")
+        return urls
+
