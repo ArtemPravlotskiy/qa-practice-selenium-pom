01033400b86414c9514897bdb36240b6c60461cf
Artsiom Praulotski <144160855+ArtemPravlotskiy@users.noreply.github.com>
2025-08-24 19:43:15 +0300

    Add Inputs_page.py

---
+++ pages/Inputs_page.py
@@ -0,0 +1,54 @@
+import logging
+
+from pages.Base_page import BasePage
+
+logger = logging.getLogger(__name__)
+
+class InputPage(BasePage):
+
+    PAGE_URL = "https://www.qa-practice.com/elements/input/simple"
+
+    """Locators"""
+    INPUT_LINK_LOCATOR = ("link text", "Inputs")
+    TEXT_INPUT_LINK_LOCATOR = ("css selector", "#content > ul > li.tab.active > a")
+    EMAIL_FIELD_LINK_LOCATOR = ("css selector", "#content > ul > li:nth-child(2) > a")
+    PASSWORD_FIELD_LINK_LOCATOR = ("css selector", "#content > ul > li:nth-child(3) > a")
+    REQUIREMENTS_BUTTON_LOCATOR = ("id", "req_header")
+    TEXT_INPUT_LOCATOR = ("id", "id_text_string")
+    EMAIL_INPUT_LOCATOR = ("id", "id_email")
+    PASSWORD_INPUT_LOCATOR = ("id", "id_password")
+
+    def open(self):
+        self.driver.get(self.PAGE_URL)
+        logger.info(f"Open page: {self.PAGE_URL}")
+
+    def open_text_input(self):
+        self.click_with_wait(self.TEXT_INPUT_LINK_LOCATOR)
+        logger.info(f"Click on element {self.TEXT_INPUT_LINK_LOCATOR} at {self.driver.current_url}")
+
+    def open_email_field(self):
+        self.click_with_wait(self.EMAIL_FIELD_LINK_LOCATOR)
+        logger.info(f"Click on element {self.EMAIL_FIELD_LINK_LOCATOR} at {self.driver.current_url}")
+
+    def open_password_field(self):
+        self.click_with_wait(self.PASSWORD_FIELD_LINK_LOCATOR)
+        logger.info(f"Click on element {self.PASSWORD_FIELD_LINK_LOCATOR} at {self.driver.current_url}")
+
+    def open_requirements(self):
+        self.click_with_wait(self.REQUIREMENTS_BUTTON_LOCATOR)
+        logger.info(f"Click on element {self.REQUIREMENTS_BUTTON_LOCATOR} at {self.driver.current_url}")
+
+    def input_text_to_field(self, locator, value: str):
+        element = self.find(locator)
+        element.clear()
+        element.send_keys(value)
+        logger.info(f"Input '{value}' into element {locator} on {self.driver.current_url}")
+
+    def input_text(self, line: str):
+        self.input_text_to_field(self.TEXT_INPUT_LOCATOR, line)
+
+    def input_email(self, line: str):
+        self.input_text_to_field(self.EMAIL_INPUT_LOCATOR, line)
+
+    def input_password(self, line: str):
+        self.input_text_to_field(self.PASSWORD_INPUT_LOCATOR, line)
