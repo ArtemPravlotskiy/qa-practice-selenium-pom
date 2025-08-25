39c458a9d35e2a1aa97786f83eb18c577b7d4d39
Artsiom Praulotski <144160855+ArtemPravlotskiy@users.noreply.github.com>
2025-08-25 23:40:14 +0300

    Change driver-manager logic, add req.txt

--- conftest.py
+++ conftest.py
@@ -11,7 +11,6 @@ from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
-from webdriver_manager.core.driver_cache import DriverCacheManager
 
 def pytest_configure(config):
     level = logging.DEBUG if config.getoption("debug") else logging.INFO
@@ -27,6 +26,8 @@ def pytest_configure(config):
 
 def pytest_addoption(parser):
     """Params for pytest command line"""
+    parser.addoption("--use-manager", action="store_true",
+                     help="Use webdriver-manager to download drivers")
     parser.addoption("--browser", action="store", default="chrome",
                      help="Available browsers: chrome(default), firefox, edge, all")
     parser.addoption("--parallel", action="store_true", default=False,
@@ -41,54 +42,52 @@ def pytest_addoption(parser):
 def driver(request):
     browser = request.param
     headless = request.config.getoption("headless")
-    # cache_manager = DriverCacheManager(root_dir="./.wdm_cache")
+    use_manager = request.config.getoption("use_manager")
 
     if browser == "chrome":
-        logging.debug("Start creation Chrome WebDriver")
         options = webdriver.ChromeOptions()
         options.add_argument("--window-size=1920,1080")
-
         if headless:
             options.add_argument("--headless=new")
-            logging.debug("Turn on headless mode")
 
-        # service = ChromeService(executable_path=ChromeDriverManager(cache_manager=cache_manager).install())
-        driver = webdriver.Chrome(options=options)
-        # driver = webdriver.Chrome(service=service, options=options)
+        if use_manager:
+            service = ChromeService(executable_path=ChromeDriverManager().install())
+            driver = webdriver.Chrome(service=service, options=options)
+        else:
+            driver = webdriver.Chrome(options=options)
 
     elif browser == "firefox":
-        logging.debug("Start creation Firefox WebDriver")
         options = webdriver.FirefoxOptions()
         options.add_argument("--window-size=1920,1080")
-
         if headless:
             options.add_argument("--headless")
-            logging.debug("Turn on headless mode")
 
-        service = FirefoxService(executable_path=GeckoDriverManager().install())
-        driver = webdriver.Firefox(service=service, options=options)
+        if use_manager:
+            service = FirefoxService(executable_path=GeckoDriverManager().install())
+            driver = webdriver.Firefox(service=service, options=options)
+        else:
+            driver = webdriver.Firefox(options=options)
 
     elif browser == "edge":
-        logging.debug("Start creation Edge WebDriver")
         options = webdriver.EdgeOptions()
         options.add_argument("--window-size=1920,1080")
-
         if headless:
             options.add_argument("--headless=new")
-            logging.debug("Turn on headless mode")
 
-        service = EdgeService(executable_path=EdgeChromiumDriverManager().install())
-        driver = webdriver.Edge(service=service, options=options)
+        if use_manager:
+            service = EdgeService(executable_path=EdgeChromiumDriverManager().install())
+            driver = webdriver.Edge(service=service, options=options)
+        else:
+            driver = webdriver.Edge(options=options)
 
     else:
-        logging.error("Unsupported browser selected")
         raise ValueError(f"Unsupported browser: {browser}")
 
     driver.implicitly_wait(5)
     request.cls.driver = driver
-
     yield driver
     driver.quit()
+
 
 def pytest_generate_tests(metafunc):
     """What browsers should be tested"""
